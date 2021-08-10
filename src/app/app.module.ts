b233fb30b6c68ba0383f01bd233ca47971ad3be0
unknown <lathakoduri994@gmail.com>
2021-08-10 18:01:53 -0400

    navigating with router links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,7 +15,7 @@ import { Router, RouterModule, Routes } from '@angular/router';
 
 const appRoutes: Routes = [
 { path : '', component: HomeComponent}, 
-{ path : 'users', component: UserComponent},
+{ path : 'users', component: UsersComponent},
 { path : 'servers', component: ServersComponent} ,
 ];
 
