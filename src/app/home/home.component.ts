bdd32e95fbf6071fc260799f6a54d99ecd597b13
unknown <lathakoduri994@gmail.com>
2021-08-23 16:59:49 -0400

    using a fake auth service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-home',
@@ -8,13 +9,22 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private route:Router) { }
+  constructor(private route:Router,
+              private authService: AuthService) { }
 
   ngOnInit() {
   }
 
   onLoadServer(id: number){
     this.route.navigate(['/servers',id,'edit'], {queryParams: {allowEdit: '1'}, fragment:'loading'});
-
   }
+
+  onLogin(){
+   this.authService.login();
+  };
+
+  onLogout(){
+  this.authService.logout();
+  };
+
 }
