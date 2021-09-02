da866bf1b0bcaa29783719e72d18d9f126e34c65
unknown <lathakoduri994@gmail.com>
2021-09-02 17:32:29 -0400

    understanding location stratagies

--- src/app/error-message/error-message.component.ts
+++ src/app/error-message/error-message.component.ts
@@ -11,7 +11,7 @@ errorMessage : string;
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.errorMessage = this.route.snapshot.data['message'];
+    // this.errorMessage = this.route.snapshot.data['message'];
     this.route.data.subscribe(
       (data: Data)=>{
         this.errorMessage = data['message']
